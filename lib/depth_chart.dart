9a49cb4f33e1ae8305a2e599259571c2db0a6054
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-10-05 16:39:30 -0300

    fix: fixes DepthChart's onPress highlight when widget doesn't fill the entire X axis

--- lib/depth_chart.dart
+++ lib/depth_chart.dart
@@ -32,12 +32,12 @@ class _DepthChartState extends State<DepthChart> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onLongPressStart: (details) {
-        pressOffset = details.globalPosition;
+        pressOffset = details.localPosition;
         isLongPress = true;
         setState(() {});
       },
       onLongPressMoveUpdate: (details) {
-        pressOffset = details.globalPosition;
+        pressOffset = details.localPosition;
         isLongPress = true;
         setState(() {});
       },
